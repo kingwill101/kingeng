e1419f2719cda4a928d92290e573778d83ad667c
Glenford <glenfordwilliams@gmail.com>
2014-01-10 10:56:50 -0500

    added documentation

--- audio.h
+++ audio.h
@@ -16,11 +16,11 @@ public:
 
 private:
     void initPlayer();
-    libvlc_instance_t *inst;
-    libvlc_media_player_t *mp;
-    libvlc_media_t *m;
-    bool isPlaying;
-    bool paused;
+    libvlc_instance_t *inst; //holds our libvlc instance
+    libvlc_media_player_t *mp; //media player
+    libvlc_media_t *m; // stores data for each media object
+    bool isPlaying; // is the media player playing anything
+    bool paused; // is the media player currently paused
 };
 
 #endif // AUDIO_H
